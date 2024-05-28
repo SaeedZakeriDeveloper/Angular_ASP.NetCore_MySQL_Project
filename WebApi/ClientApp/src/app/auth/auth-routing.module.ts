8e3dd13830054ca6668ab9b72ce35e5e7b0aa4db
Saeed Zakeri <saeed.zakeri.dev@gmail.com>
2024-05-28 20:48:30 +0330

    edited codebase.

--- WebApi/ClientApp/src/app/auth/auth-routing.module.ts
+++ WebApi/ClientApp/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
@@ -19,4 +18,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule {}
+export class AuthRoutingModule {
+}
--- WebApi/ClientApp/src/app/auth/auth.module.ts
+++ WebApi/ClientApp/src/app/auth/auth.module.ts
@@ -1,10 +1,9 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { SharedModule } from '../shared';
-import { AuthRoutingModule } from './auth-routing.module';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {SharedModule} from '../shared';
+import {AuthRoutingModule} from './auth-routing.module';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 @NgModule({
   imports: [
@@ -16,4 +15,5 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
